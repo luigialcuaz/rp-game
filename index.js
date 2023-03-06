1f25f199a5e60e228064d3060f51af23906f101c
Luigi Alcuaz <alcuazluigi@gmail.com>
2023-03-05 19:30:34 -0800

    Added an attack() function, attack button and changed initial render of cards to display empty dice.

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import Character from "./Character.js";
 import characterData from "./data.js";
 
+function attack() {
+  console.log("attack");
+}
+
 function render() {
   document.getElementById("hero").innerHTML = wizard.getCharacterHtml();
   document.getElementById("monster").innerHTML = orc.getCharacterHtml();
 }
 
+document.getElementById("attack-button").addEventListener("click", attack);
 const wizard = new Character(characterData.hero);
 const orc = new Character(characterData.monster);
 
